1c26e4283015c2f298e2dd3a3398825788a85d0e
saintbassanaga <saintbassanaga01@gmail.com>
2023-05-03 03:16:07 -0700

    updating app.py

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,18 +7,54 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+from typing import Tuple
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+
+if getenv('AUTH_TYPE') == 'auth':
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+elif getenv('AUTH_TYPE') == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+
+
+@app.before_request
+def before_request():
+    """Check user permission."""
+    if not auth:
+        return
+    if not auth.require_auth(request.path,
+                             ['/api/v1/status/',
+                              '/api/v1/unauthorized/', '/api/v1/forbidden/']):
+        return
+    if not auth.authorization_header(request):
+        abort(401)
+    if not auth.current_user(request):
+        abort(403)
 
 
 @app.errorhandler(404)
-def not_found(error) -> str:
+def not_found(error) -> Tuple[str, int]:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(401)
+def unauthorized(error) -> Tuple[str, int]:
+    """Custom error handler for unauthorized request."""
+    return jsonify({'error': 'Unauthorized'}), 401
+
+
+@app.errorhandler(403)
+def forbidden(error) -> Tuple[str, int]:
+    """Custom error handler for unauthorized request."""
+    return jsonify({'error': 'Forbidden'}), 403
 
 
 if __name__ == "__main__":
