932e84e0270e27640dd875fcf242443cd19b6ab0
saintbassanaga <saintbassanaga01@gmail.com>
2023-05-03 02:24:25 -0700

    preset of project with the given repository

---
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+""" Module of Index views
+"""
+from flask import jsonify, Response
+
+from api.v1.views import app_views
+
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status() -> Response:
+    """ GET /api/v1/status
+    Return:
+      - the status of the API
+    """
+    return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats/', strict_slashes=False)
+def stats() -> Response:
+    """ GET /api/v1/stats
+    Return:
+      - the number of each objects
+    """
+    from models.user import User
+    stat = {'users': User.count()}
+    return jsonify(stat)
